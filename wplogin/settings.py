0a5aaf8f35387a7a8482dcc71b8b1801eb316600
Makan DIANKA <makan.dianka@hotmail.com>
2025-04-14 19:41:52 +0200

    setup staticfiles

--- wplogin/settings.py
+++ wplogin/settings.py
@@ -124,10 +124,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img/')
+
+STATIC_ROOT = (
+    os.path.join(BASE_DIR, 'staticfiles')
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
