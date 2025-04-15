2e2809e1c2493c2b4cb1378b8fb12c98f198c3b6
Makan DIANKA <makan.dianka@hotmail.com>
2025-04-15 22:07:19 +0200

    refactory and download favicon

--- target.py
+++ target.py
@@ -16,34 +16,62 @@ def get_commandline():
 
 
 
-def website(url):
+def is_reachable(url):
     try:
-        res = requests.get(url)
+        http_response = requests.get(url)
     except requests.exceptions.MissingSchema as e:
         print(e)
     except Exception as e:
         print(e)
     else:
-        if res.ok:
-            soup = BeautifulSoup(res.text, 'html.parser')
-            title = soup.find('title')
-            return {'title' : title.text, 'url': res.url, 'code': res.status_code}
+        if http_response.ok:
+            return http_response
         else:
-            return {'code': res.status_code}
+            raise f"{url} - Is not reachable. status_code {http_response.status_code}"
+
+
+def download_icon(icon_url):
+    img_data = is_reachable(icon_url).content
+    img = icon_url.split('/')[-1]
+    # img_name, img_ext = img.split('.')
+
+    with open(f'static/img/{img}', 'wb') as f:
+        f.write(img_data)
+
+    return img
+
+
+
+def find_icon(soup):
+    icon = soup.find('link', rel=lambda x: x and "icon" in x.lower())
+    if icon and icon.get("href"):
+        favicon_url = icon["href"]
+        if not favicon_url.startswith("http"):
+            favicon_url = url + favicon_url
+        icon_name = download_icon(favicon_url)
+        return icon_name
+
+
+
+def website(url):
+    http_response = is_reachable(url)
+    soup = BeautifulSoup(http_response.text, 'html.parser')
+    icon_name = find_icon(soup)
+    title = soup.find('title')
+
+    return {'title' : title.text, 'url': http_response.url, 'icon': icon_name, 'code': http_response.status_code}
+
 
 
 def generate_conf_file():
     command = get_commandline()
     site = website(command.url)
     
-    print(site)
-    if site['code'] != 200:
-        raise "Error"
-
     data = {
         'title' : site['title'],
         'url' : site['url'],
-        'code' : site['code']
+        'code' : site['code'],
+        'icon' : site['icon'],
     }
 
     with open('conf.json', 'w', encoding='utf-8') as conf:
